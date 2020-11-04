3d9a690baf4b309b342f3794b73879451871bf98
elizarkozik <72994115+elizarkozik@users.noreply.github.com>
2020-11-04 08:46:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,103 @@
+//fråga user att mata in 2 siffror. Så här skriver man kommentar i java cript.
+
+// vi kommer att visa suman av dem 2 siffrorna 
+//användare har matat in. 
+
+// fråga användare ?? med promt ()
+// istället för form/input ska vi använda prompt
+
+
+
+// vi ska lagra info/data: i var // string 
+        //convertera till int
+
+   /* var number1= prompt("Mata in en siffra");
+    var number2= prompt("mata in andra siffran");
+    var number3= prompt("mata in tredje siffran");
+
+    var convertedNumber1= Number(number1);
+    var convertedNumber2= Number(number2);
+    var convertedNumber3= Number(number3);
+
+    // plusa/addera dem
+    
+    var summan=  convertedNumber1 * convertedNumber2 * convertedNumber3 
+    var summan=  convertedNumber1 * convertedNumber2 * convertedNumber3 
+    // vi tar inte emot någon minus siffror 
+    var summan=   Number (number1) * Number(number2)*Number (number3) 
+    var summan=   Number (number1) * Number(number2) *Number (number3) 
+
+
+   alert (summan);
+        
+        //visa till användare. det måste stå samma sak på var och alert (). 
+
+        // *************** //
+        //program 2: ta in betygsinfo från användare och 
+        // lagar in i en lista.
+        
+    
+    
+       /* var fysikBetygsGrad = prompt("mata in fysiks betyg");
+        var matteBetygsGrad = prompt("mata in Mattes betyg");
+        var webBetygsGrad = prompt("mata in webs betyg");
+        var historiaBetygsGrad = prompt("mata in historias betyg");
+        var kemiBetygsGrad = prompt("mata in kemis betyg");
+        
+        
+       // Vi ska inte ta något betyg som är mindre än 2.
+         
+{ 
+       
+       var betygsLista = [fysikBetygsGrad, 
+        kemiBetygsGrad, 
+        webBetygsGrad,
+        historiaBetygsGrad,
+         matteBetygsGrad]
+       }  
+   
+
+       //----------------
+
+
+       
+// rad efter rad översätts 
+ 
+// Vi ska inte ta något betyggrad som är mindre än 2 
+/* var fysikBetygsGrad = prompt("mata in fysiks betyg");
+ 
+if(fysikBetygsGrad >=2 ) {
+var betygsLista = [fysikBetygsGrad]
+ //alert(betygsLista) 
+ alert("Grattis " + betygsLista)
+}
+else if(fysikBetygsGrad >=3) { 
+ 
+ alert("Bra jobbat")
+}
+else {
+ alert("betyget är för lågt")
+}
+
+
+    
+// Vi ska inte ta något betyg som är mindre än 2      
+// varje if statmemnt körs/översätts av maskin
+//else if körs bara om ovanstående vilkoret inte är sant.
+
+
+/* if  (fysikBetygsGrad=>4){
+    alert ("väldigt bra")
+}
+if ( fysikBetygsGrad=>3) {
+alert ("du har ganska bra")
+
+}
+
+(false) 
+    alert ("hej, den här raden komer att köras om övanstående vilkoret inte sttämmer")
+    
+
+
+// hur ska vi hämta value från en array*/
+//var infoGradeKemi = 5;
---
+++ app2.js
@@ -0,0 +1,20 @@
+
+    var number1= prompt("Mata in en siffra");
+    var number2= prompt("mata in andra siffran");
+    var number3= prompt("mata in  den trejde siffran");
+
+
+    var convertedNumber1= Number(number1);
+    var convertedNumber2= Number(number2);
+    var convertedNumber3= Number(number3);
+
+
+    // plusa/addera dem
+    
+   var summan=  convertedNumber1 - (convertedNumber2 + convertedNumber3) 
+    // vi tar inte emot någon minus siffror 
+   var summan=   Number (number1) - (Number(number2) + Number(number3))
+   var summan=   Number (number1) - (Number(number2) + Number(number3))
+
+   alert (summan);
+ 
